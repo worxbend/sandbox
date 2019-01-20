487a0f87f6105fd4cde9841eab8b945e63e49f19
Alexander Balyshyn <balyszyn@gmail.com>
2019-01-20 20:57:06 +0200

    naming refactoring

--- stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouteModule.scala
+++ stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouteModule.scala
@@ -1,9 +1,10 @@
 package io.kzonix.sird
 
-import play.api.{ Configuration, Environment }
+import play.api.inject.Binding
+import play.api.{Configuration, Environment}
 
 class RouteModule extends play.api.inject.Module {
-  def bindings(environment: Environment, configuration: Configuration) = {
+  def bindings(environment: Environment, configuration: Configuration) : Seq[Binding[_]] = {
     Seq(
       bind[RouterProvider].to[SirdProvider])
   }
--- stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouterProvider.scala
+++ stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouterProvider.scala
@@ -3,6 +3,4 @@ package io.kzonix.sird
 import javax.inject.Provider
 import play.api.routing.Router
 
-trait RouterProvider extends Provider[Router] {
-
-}
+trait RouterProvider extends Provider[Router] {}
