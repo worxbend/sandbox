17f179597fc34721cddc04e0042e789ce11a1c44
Alexander Balyshyn <balyszyn@gmail.com>
2019-01-21 18:04:31 +0200

    add scaladoc

--- stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouterProvider.scala
+++ stack/playframework/sird-provider/src/main/scala/io/kzonix/sird/RouterProvider.scala
@@ -3,4 +3,7 @@ package io.kzonix.sird
 import javax.inject.Provider
 import play.api.routing.Router
 
+/**
+  * Provides a fully-constructed, composed instance of [[Router]].
+  **/
 trait RouterProvider extends Provider[Router] {}
