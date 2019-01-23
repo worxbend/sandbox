26077080614e38ace94df60a63f7d88590f58c10
Alexander Balyshyn <balyszyn@gmail.com>
2019-01-23 23:06:11 +0200

    add scaladoc

--- stack/playframework/sird-provider-api/src/main/scala/io/kzonix/sird/ProvidedRouter.scala
+++ stack/playframework/sird-provider-api/src/main/scala/io/kzonix/sird/ProvidedRouter.scala
@@ -2,17 +2,17 @@ package io.kzonix.sird
 
 import play.api.routing.Router
 
-/** Provides */
+/** Uses for definition String Interpolation Route Definition */
 trait ProvidedRouter extends Router {
 
   /**
     * Route path. Can be overrode in implementation of [[Router]].
-    * */
+    **/
   def routePrefix: String = ""
 
   /**
     * Route version. Can be overrode in implementation of [[Router]].
-    * */
+    **/
   def routeVersion: Int = 0
 
   private lazy val versioned = routeWithVersion(routePrefix)(_)
