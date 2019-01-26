80c2fa0dd17ffa13d17af1d047f2e1dfc13b3c3c
Alexander Balyshyn <balyszyn@gmail.com>
2019-01-26 23:55:42 +0200

    refactoring, configure logging

--- stack/playframework/index-service/src/main/scala/io/kzonix/index/controllers/IndexController.scala
+++ stack/playframework/index-service/src/main/scala/io/kzonix/index/controllers/IndexController.scala
@@ -1,17 +1,17 @@
 package io.kzonix.index.controllers
 
-import javax.inject.{Inject, Singleton}
-import play.api.libs.json.JsValue
-import play.api.mvc.{AbstractController, Action, ControllerComponents}
+import javax.inject.{ Inject, Singleton }
+import play.api.mvc.{ AbstractController, Action, AnyContent, ControllerComponents }
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton
-class IndexController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class IndexController @Inject() (cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
 
-  def index: Action[JsValue] = Action(parse.json).async {
-    implicit request => {
-      Future(Ok(s"Hello world $request"))
-    }
+  def index: Action[AnyContent] = Action.async {
+    implicit request =>
+      {
+        Future(Ok(s"Hello world $request"))
+      }
   }
 }
--- stack/playframework/play-utile/src/main/scala/io/kzonix/play/SimpleApplicationLoader.scala
+++ stack/playframework/play-utile/src/main/scala/io/kzonix/play/SimpleApplicationLoader.scala
@@ -3,17 +3,16 @@ package io.kzonix.play
 import io.kzonix.sird.SirdProvider
 import play.api.ApplicationLoader
 import play.api.inject.bind
-import play.api.inject.guice.{GuiceApplicationLoader, GuiceableModule}
+import play.api.inject.guice.{ GuiceApplicationLoader, GuiceableModule }
 import play.api.routing.Router
 
 /**
-  * An ApplicationLoader that uses Guice to bootstrap the application.
-  * It bind [[Router]] to [[SirdProvider]].
-  */
+ * An ApplicationLoader that uses Guice to bootstrap the application.
+ * It bind [[Router]] to [[SirdProvider]].
+ */
 class SimpleApplicationLoader extends GuiceApplicationLoader {
 
   protected override def overrides(context: ApplicationLoader.Context): Seq[GuiceableModule] = {
-    Console.println("LOG")
     super.overrides(context) :+ (bind[Router].toProvider[SirdProvider]: GuiceableModule)
   }
 
