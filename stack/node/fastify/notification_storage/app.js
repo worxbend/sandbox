14e7a6d83f063219891565e6e77dd037565761c6
Alexander Balyshyn <balyszyn@gmail.com>
2019-02-18 22:09:22 +0200

    update app.js

--- stack/node/fastify/notification_storage/app.js
+++ stack/node/fastify/notification_storage/app.js
@@ -8,19 +8,10 @@ const initMongo = require('./mongoose-init')
 module.exports = async (fastify, opts) => {
   // Initialization of mongo-client.
   await initMongo(opts)
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
   fastify.register(PluginRegistry)
-
-  // This loads all plugins defined in services
-  // define your routes in one of these
   // TODO: replace with currated routes loading.
-  // Encapsulate current way of auto-loading in separate components to provide possibility of versioning and grouping of route-configuration
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'services'),
     options: Object.assign({}, opts)
   })
-
-  // Make sure to call next when done
 }
