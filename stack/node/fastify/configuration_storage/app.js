77dec902fcba683789379cb8d1ff9e8412b10ce2
limpid-kzonix <balyszyn@gmail.com>
2019-04-03 21:03:44 +0300

    fix formatting issues

--- stack/node/fastify/configuration_storage/app.js
+++ stack/node/fastify/configuration_storage/app.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const path = require( 'path' )
-const AutoLoad = require( 'fastify-autoload' )
-const initMongo = require( './mongoose-init' )
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
+const initMongo = require('./mongoose-init')
 
-module.exports = async ( fastify, opts ) => {
+module.exports = async (fastify, opts) => {
   // Place here your custom code!
   let configuration = {
     'mongo.host': 'localhost',
@@ -15,21 +15,21 @@ module.exports = async ( fastify, opts ) => {
     ...opts,
     ...configuration
   }
-  await initMongo( fastify, opts )
+  await initMongo(fastify, opts)
   // Do not touch the following lines
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
 
-  fastify.register( AutoLoad, {
-    dir: path.join( __dirname, 'plugins' ),
-    options: Object.assign( {}, opts )
-  } )
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
 
   // This loads all plugins defined in services
   // define your routes in one of these
-  fastify.register( AutoLoad, {
-    dir: path.join( __dirname, 'services' ),
-    options: Object.assign( {}, opts )
-  } )
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'services'),
+    options: Object.assign({}, opts)
+  })
 }
--- stack/node/fastify/configuration_storage/helpers/fs-helper.js
+++ stack/node/fastify/configuration_storage/helpers/fs-helper.js
@@ -1,68 +1,8 @@
 'use strict'
-const fse = require('fs-extra')
-const path = require('path')
-const AdmZip = require('adm-zip')
+function init () {
 
-class FileSystemHelper {
-  /**
-   * Write file.
-   * @param {String} dirPath Absolute destination path.
-   * @param {String} filename  File name with extension.
-   * @param {Buffer} data  Data for writing.
-   * @returns Object which contains file metadata.
-   */
-  static async writeFile(dirPath, filename, data) {
-    await FileSystemHelper._writeFile(dirPath, filename, data)
-  }
-
-  static async writeZipFile(dirPath, zipFileName, {
-    innerFileName,
-    buffer
-  }) {
-    let newZipFile = new AdmZip()
-    newZipFile.addFile(innerFileName, buffer)
-    let createdZipBuffer = newZipFile.toBuffer()
-    newZipFile.writeZip(dirPath, zipFileName)
-    return createdZipBuffer
-  }
-
-  static _writeFile(dirPath, filename, buffer) {
-    return new Promise((resolve, reject) => {
-      fse.outputJSON()
-    })
-  }
-
-  static _readFile(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(data)
-        }
-      })
-    })
-  }
-
-  static _deleteFile(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve(true)
-        }
-      })
-    })
-  }
-
-  static _deleteDirectory(dirPath) {
-    return new Promise((resolve, reject) => {
-      fs.
-    })
-  }
 }
 
 module.exports = {
-  FileSystemHelper
+  init
 }
