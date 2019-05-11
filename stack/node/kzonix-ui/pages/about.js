94e499ea45b8e8ec537448e59d071d1ef0a1509e
limpid-kzonix <balyszyn@gmail.com>
2019-05-11 20:34:23 +0300

    Test blue-ocean cd/ci

--- stack/node/kzonix-ui/pages/about.js
+++ stack/node/kzonix-ui/pages/about.js
@@ -1,8 +1,10 @@
 import withHeader from '../components/header/layout-fn'
 
-const pageContent = () => <p>
-                            About us !
-                          </p>
+const pageContent = () => (
+  <p>
+    About us !
+  </p>
+)
 export default function About () {
   return withHeader(pageContent)()
 }
