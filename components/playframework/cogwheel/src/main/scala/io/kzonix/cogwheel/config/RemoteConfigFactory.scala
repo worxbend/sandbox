8fefb3eea7cd1a417d5a9550c17157ceaa639b40
limpid-kzonix <balyszyn@gmail.com>
2021-06-17 23:49:41 +0300

    minor enhancement of config factory implementation

--- components/playframework/cogwheel/src/main/scala/io/kzonix/cogwheel/config/RemoteConfigFactory.scala
+++ components/playframework/cogwheel/src/main/scala/io/kzonix/cogwheel/config/RemoteConfigFactory.scala
@@ -1,6 +1,7 @@
 package io.kzonix.cogwheel.config
 
 import com.typesafe.config._
+import com.typesafe.scalalogging.LazyLogging
 import io.circe.CursorOp.DownField
 import io.circe.DecodingFailure
 import io.kzonix.cogwheel.aws.AWSParameterStoreClient
@@ -8,30 +9,56 @@ import io.kzonix.cogwheel.config.ConfigPathUtils.PathUtils._
 import io.kzonix.cogwheel.config.parser.CompositeKeyParser
 import io.kzonix.cogwheel.config.parser.SimpleKeyParser
 import io.kzonix.cogwheel.config.parser.ValueParser
+import org.checkerframework.checker.units.qual.s
+
+import scala.util.Failure
+import scala.util.Try
 
 class RemoteConfigFactory(
     parameterStoreClient: AWSParameterStoreClient
-) {
+) extends LazyLogging {
 
   def loadConfig(namespace: String, configKey: String): Config = {
     val paramPath                   = toParamName(configKey).withConfigNameSpace(namespace)
+    val rootConfigKey               = toConfigKey(paramPath)
+    logger.info(s"Loading config by key $rootConfigKey")
     val params: Map[String, String] = parameterStoreClient.fetchParameters(paramPath)
+    logger.info(s"Loaded ${ params.size } key-value pair(s)")
     val parsedParams                = params.map { case (path, value) => path -> ValueParser.parseStringValue(value) }
-    val failedParams                = parsedParams.filter { case (_, triedConfigValue) => triedConfigValue.isFailure }
+    val successParams               = validateOrThrow(parsedParams)
+
+    parseConfig(successParams).getObject(rootConfigKey).toConfig
+  }
+
+  private def parseConfig(params: Map[String, ConfigValue]) = {
+    logger.info(s"Parsing ${ params.size } param(s)...")
+    logger.whenDebugEnabled {
+      logger.debug(params.toString)
+    }
+
+    new CompositeKeyParser(new SimpleKeyParser())
+      .parse(params)
+      .toConfig
+  }
+
+  private def validateOrThrow(parsedParams: Map[String, Try[ConfigValue]]): Map[String, ConfigValue] = {
+    val failedParams = parsedParams.filter { case (_, parsedRes) => parsedRes.isFailure }
     if (failedParams.nonEmpty) {
       val downFields = failedParams.map {
-        case (path, _) =>
-          // log error for more details even if typesafe config provides bad explanation of error.
-          DownField(path)
+        case (path, Failure(exception)) =>
+          s
+          logger.error(
+            s"Failed entry with key '${ toConfigKey(path) }':",
+            exception
+          )
+          DownField(toConfigKey(path))
       }
       throw DecodingFailure(
         s"Failed to parse configuration at key: ",
         downFields.toList
       )
-    }
-    new CompositeKeyParser(new SimpleKeyParser())
-      .parse(parsedParams.map { case (path, success) => path -> success.get })
-      .toConfig
+    } else parsedParams.map { case (path, success) => path -> success.get }
+
   }
 
 }
